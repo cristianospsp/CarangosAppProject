b856de7350bc925455819edde5a56d392c972ab0
Cristiano de Oliveira <cristianospsp@gmail.com>
2015-10-17 17:21:24 -0300

    ajuste no codigo

--- CarangosApp/src/main/java/br/com/caelum/fj59/carangos/CarangosApplication.java
+++ CarangosApp/src/main/java/br/com/caelum/fj59/carangos/CarangosApplication.java
@@ -1,5 +1,6 @@
 package br.com.caelum.fj59.carangos;
 
+import android.app.Activity;
 import android.app.Application;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
@@ -9,12 +10,15 @@ import java.util.List;
 
 import br.com.caelum.fj59.carangos.infra.MyLog;
 import br.com.caelum.fj59.carangos.modelo.Publicacao;
+import br.com.caelum.fj59.carangos.tasks.RegistraAparelhoTask;
 
 /**
  * Created by android5372 on 10/10/15.
  */
 public class CarangosApplication extends Application {
 
+    private static final String ID_DO_REGISTRO = "idDoRegistro";
+    private static final String REGISTRADO = "registradoNoGcm";
     private List<Publicacao> publicacoes = new ArrayList<>();
     private List<AsyncTask<?,?,?>> tasks = new ArrayList<AsyncTask<?,?,?>>();
     private SharedPreferences preferences;
@@ -25,6 +29,25 @@ public class CarangosApplication extends Application {
         for (AsyncTask t : this.tasks) {
             t.cancel(true);
         }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        preferences = getSharedPreferences("configs", Activity.MODE_PRIVATE);
+        registraNoGCM();
+    }
+
+    public void registraNoGCM() {
+        if (!usuarioRegistrado()){
+            new RegistraAparelhoTask(this).execute();
+        }else {
+            MyLog.i("Aparelho jah cadastrado! Sei id é: " + preferences.getString(ID_DO_REGISTRO, null));
+        }
+    }
+
+    private boolean usuarioRegistrado() {
+        return preferences.getBoolean(REGISTRADO, false);
     }
 
     public void registra(AsyncTask<?,?,?> task) {
@@ -43,8 +66,8 @@ public class CarangosApplication extends Application {
     public void lidaComRespostaDoRegistroNoServidor(String registro) {
         if (registro != null) {
             SharedPreferences.Editor editor = preferences.edit();
-            editor.putBoolean("REGISTRADO", true);
-            editor.putString("ID_DO_REGISTRO", registro);
+            editor.putBoolean(REGISTRADO, true);
+            editor.putString(ID_DO_REGISTRO, registro);
             editor.commit();
         }
     }
