0210fe292bcbf73b91ee93bfb99303e7d144d570
Cristiano de Oliveira <cristianospsp@gmail.com>
2015-10-24 16:29:29 -0200

    criado projeto CarangosHotRod

---
+++ CarangosHotRod/src/main/java/br/com/caelum/fj59/carangostunning/activity/HotRodComprasActivity.java
@@ -0,0 +1,9 @@
+package br.com.caelum.fj59.carangostunning.activity;
+
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by android5372 on 24/10/15.
+ */
+public class HotRodComprasActivity extends ActionBarActivity {
+}
